778095edb330788119b7bc839782934b3a8a1ac2
moltak <moltak@gmail.com>
2016-06-12 16:54:19 +0900

    Add gulp electron and gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@
 
 let gulp = require('gulp');
 let server = require('gulp-express');
+let shell = require('gulp-shell');
+let del = require('del');
+
 let paths = {
 	scripts: ['app.js', 'routes/**/*.js', 'public/**/*.js', 'gulpfile.js']
 };
+
+gulp.task('clean', () => {
+	return del(['build']);
+});
 
 gulp.task('run', () => {
 	server.run(['bin/www']);
@@ -15,3 +22,8 @@ gulp.task('watch', () => {
 });
 
 gulp.task('default', ['watch', 'run']);
+
+gulp.task('electron', ['clean'], () => {
+	return gulp.src(['main.js', './views/**/**', './public/**/**'])
+		.pipe(gulp.dest('build/electron'));
+});
